6e821c8d428cb51f86fe7469126b4e77f806df10
david20571015 <david20571015@gmail.com>
2019-12-14 01:56:22 +0800

    finish problems

--- building.cpp
+++ building.cpp
@@ -8,30 +8,33 @@ Building::Building()
     simu_timer = new QTimer();
     connect(simu_timer,SIGNAL(timeout()),this,SLOT(update()));
 
-//    floor[1]=new Floor(new Add1());
-//    floor[2]=new Floor(new Add1());
-//    floor[3]=new Floor(new Add1());
-//    floor[4]=new Floor(new Add1());
-//    floor[5]=new Floor(new Add1());
-//    floor[6]=new Floor(new Add1());
-//    floor[7]=new Floor(new Add1());
-//    floor[8]=new Floor(new Add1());
-//    floor[9]=new Floor(new Add1());
-//    floor[10]=new Floor(new Add1());
-    floor[0] = new Floor(new Add1());
-    floor[1] = new Floor(new Prime());
-    floor[2] = new Floor(new LongestPair());
-    floor[3] = new Floor(new ShyGame());
-    floor[4] = new Floor(new FibonacciOfFibonacci());
-    floor[5] = new Floor(new Minesweeper());
-    floor[6] = new Floor(new LargestRoot());
-    floor[8] = new Floor(new findfactorial());
-    floor[9] = new Floor(new shortestsummation());
-    floor[10] = new Floor(new FindThePeriodOfString());
-    floor[11] = new Floor(new LargeFactorial());
-    floor[12] = new Floor(new SumOfLargeHex());
-    floor[13] = new Floor(new DistanceBetweenPrime());
-    floor[14] = new Floor(new Escape());
+    floor[0]  = new Floor(new P1);
+    floor[1]  = new Floor(new P2);
+    floor[2]  = new Floor(new P3);
+    floor[3]  = new Floor(new P4);
+    floor[4]  = new Floor(new P5);
+    floor[5]  = new Floor(new P6);
+    floor[6]  = new Floor(new P7);
+    floor[7]  = new Floor(new P8);
+    floor[8]  = new Floor(new P9);
+    floor[9]  = new Floor(new P10);
+    floor[10] = new Floor(new P11);
+    floor[11] = new Floor(new P12);
+    floor[12] = new Floor(new P13);
+    floor[13] = new Floor(new P14);
+    floor[14] = new Floor(new P15);
+    floor[15] = new Floor(new P16);
+    floor[16] = new Floor(new P17);
+    floor[17] = new Floor(new P18);
+    floor[18] = new Floor(new P19);
+    floor[19] = new Floor(new P20);
+    floor[20] = new Floor(new P21);
+    floor[21] = new Floor(new P22);
+    floor[22] = new Floor(new P23);
+    floor[23] = new Floor(new P24);
+    floor[24] = new Floor(new P25);
+    floor[25] = new Floor(new P26);
+    floor[26] = new Floor(new P27);
 }
 
 void Building::run(int n)
