c78154642bd0da43668f8a249f67490bcdca0ca5
david20571015 <david20571015@gmail.com>
2019-12-16 02:55:31 +0800

    finish largefactorial

--- largefactorial.cpp
+++ largefactorial.cpp
@@ -6,32 +6,40 @@ LargeFactorial::LargeFactorial()
 
 string LargeFactorial::solve(const string &s)
 {
+    stringstream ss;
+    short n;
+    ss << s;
+    ss >> n;
 
-    int n = stoi(s);
-    int *num = new int[2000];
-    for (int i = 0; i < 2000; i++)
-        num[i] = 0;
-    num[0] = 1;
+    unsigned long long fac[2400] = {0};
 
-    for (int i = 1; i <= n; i++)
+    fac[0] = 1;
+    for (int i = 2; i <= n; i++)
     {
-        for (int j = 0; j < 2000; j++)
-            num[j] *= i;
-        for (int j = 0; j < 1999; j++)
-        {
-            num[j + 1] += num[j] / 10;
-            num[j] %= 10;
-        }
+        for (int j = 0; j < 2399; j++)
+            fac[j] *= i;
+        for (int j = 0; j < 2399; j++)
+            if (fac[j] >= 1e15)
+            {
+                fac[j + 1] += fac[j] / (unsigned long long)1e15;
+                fac[j] %= (unsigned long long)1e15;
+            }
     }
 
-    int digit;
-    for (digit = 1999; digit >= 0; digit--)
-        if (num[digit] != 0)
-            break;
-
+    int digit = 2399;
+    for (; digit >= 0 && !fac[digit]; digit--)
+        ;
+    stringstream tmp;
     string ans;
+
     for (; digit >= 0; digit--)
-        ans += to_string(num[digit]);
-    delete[] num;
+        tmp << fixed << setfill('0') << setw(15) << fac[digit];
+
+    tmp >> ans;
+
+    for (digit = 0; digit < ans.size() && ans[digit] == '0'; digit++)
+        ;
+    ans = ans.substr(digit, ans.size() - digit);
+
     return ans;
 }
