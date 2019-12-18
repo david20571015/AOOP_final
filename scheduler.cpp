30b3ebf24cde6b39fe09ef5ba3d14b5b857747e9
david20571015 <david20571015@gmail.com>
2019-12-18 15:32:53 +0800

    finish scheduler

--- scheduler.cpp
+++ scheduler.cpp
@@ -2,9 +2,93 @@
 
 Scheduler::Scheduler()
 {
-//    nowFloor={1,3,4,6,9,10,8,7,5,2,0};
-    nowFloor={14,14,14,14,14,14,14,14,14,14};
     index=0;
+
+}
+
+void Scheduler::setInitial(int *dest, int *people)
+{
+    for(int i=0;i<27;i++)
+    {
+        if(i+1<dest[i])
+            up.push_back(Group{i+1,dest[i],people[i]});
+        else if(i+1>dest[i])
+            down.push_back(Group{i+1,dest[i],people[i]});
+        reverse(down.begin(),down.end());
+    }
+}
+
+void Scheduler::calRoute()
+{
+    vector<Group> elevator;
+    int peopleInElevator=0;
+    int now;
+
+    while(up.size()||down.size())
+    {
+        now=1;
+        while (now<=27)
+        {
+            //leave
+            for(int i=0;i<elevator.size();i++)
+            {
+                if(elevator[i].to==now)
+                {
+                    peopleInElevator-=elevator[i].num;
+                    for(int j=0;j<elevator[i].num;i++)
+                        nowFloor.push_back(now);
+                    elevator.erase(elevator.begin()+i);
+                }
+            }
+
+            //enter
+            for(int i=0;i<up.size();i++)
+            {
+                if(up[i].from==now&&peopleInElevator<10)
+                {
+                    int n=min(up[i].num,10-peopleInElevator);
+                    peopleInElevator+=n;
+                    elevator.push_back(Group{now,up[i].to,n});
+                    up[i].num-=n;
+                    if(up[i].num==0)
+                        up.erase(up.begin()+i);
+                }
+            }
+            now++;
+        }
+
+        now=27;
+        while (now>=1)
+        {
+            //leave
+            for(int i=0;i<elevator.size();i++)
+            {
+                if(elevator[i].to==now)
+                {
+                    peopleInElevator-=elevator[i].num;
+                    for(int j=0;j<elevator[i].num;i++)
+                        nowFloor.push_back(now);
+                    elevator.erase(elevator.begin()+i);
+                }
+            }
+
+            //enter
+            for(int i=0;i<down.size();i++)
+            {
+                if(down[i].from==now&&peopleInElevator<10)
+                {
+                    int n=min(down[i].num,10-peopleInElevator);
+                    peopleInElevator+=n;
+                    elevator.push_back(Group{now,down[i].to,n});
+                    down[i].num-=n;
+                    if(down[i].num==0)
+                        down.erase(down.begin()+i);
+                }
+            }
+            now--;
+        }
+    }
+    nowFloor.push_back(0);
 }
 
 int Scheduler::getNowFloor()
--- scheduler.h
+++ scheduler.h
@@ -10,8 +10,17 @@ public:
     Scheduler();
     int getNowFloor();
     void reset();
+    void setInitial(int [27],int [27]);
+    void calRoute();
 
 private:
+    struct Group
+    {
+        int from;
+        int to;
+        int num;
+    };
+    vector<Group> up,down;
     vector<int> nowFloor;
     int index;
 };
