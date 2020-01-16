704e4c5a99056468e48601c353beab823d335eaf
liang2000 <liang2000.eed07@nctu.edu.tw>
2020-01-16 08:32:32 +0800

    all correct

--- symmetricpairs.cpp
+++ symmetricpairs.cpp
@@ -25,11 +25,24 @@ string SymmetricPairs::solve(const string &s)
     else
         st = "update CityList set Lat = Lat + Lon, Lon = Lat - Lon, lat = Lat - Lon where Id regexp '[24680]$'";
     query.exec(QString::fromStdString(st));
+
     st = "update CityList set Lat = Lon where Id regexp '" + t2 + "$'";
     query.exec(QString::fromStdString(st));
-    st = "select * from(select * from (select x1, y1 from (select * from (select * from (select round(Lon, " + t3 + ") as x1, round(Lat, " + t3 + ") as y1, Id as Id1 from CityList )as a join (select round(Lon, " + t3 + ") as x2, round(Lat, " + t3;
-    st += ") as y2, Id as Id2 from CityList)as b on x1 = y2 and x2 = y1 and Id1 != Id2)as t1 group by x1, y1 order by x1, y1) as t2)as t3 where x1 <= y1) as t4 limit " + to_string(n4 - 1) + ", 1";
-    query.exec(QString::fromStdString(st));
+
+    if(t3 == "0")
+    {
+        st = "select * from(select * from (select x1, y1 from (select * from (select * from (select round(Lon, " + t3 + ") +1-1 as x1, round(Lat, " + t3 + ") as y1, Id as Id1 from CityList )as a join (select round(Lon, " + t3 + ") +1-1 as x2, round(Lat, " + t3;
+        st += ") as y2, Id as Id2 from CityList)as b on x1 = y2 and x2 = y1 and Id1 != Id2)as t1 group by x1, y1 order by x1, y1) as t2)as t3 where x1 <= y1) as t4 limit " + to_string(n4 - 1) + ", 1";
+        query.exec(QString::fromStdString(st));
+    }
+    else
+    {
+        st = "select * from(select * from (select x1, y1 from (select * from (select * from (select round(Lon, " + t3 + ") as x1, round(Lat, " + t3 + ") as y1, Id as Id1 from CityList )as a join (select round(Lon, " + t3 + ") as x2, round(Lat, " + t3;
+        st += ") as y2, Id as Id2 from CityList)as b on x1 = y2 and x2 = y1 and Id1 != Id2)as t1 group by x1, y1 order by x1, y1) as t2)as t3 where x1 <= y1) as t4 limit " + to_string(n4 - 1) + ", 1";
+        query.exec(QString::fromStdString(st));
+    }
+
+
     query.next();
     if(query.value(0).isNull())
         ans = "NULL ";
